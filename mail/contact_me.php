0c1493498607adcb5fa7dc031541183f453e92c4
Igor Araújo <igor.araujo@fourtime.com>
2018-12-29 02:15:38 -0200

    add underline in msg
    add underline in msg

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -21,7 +21,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'contato@aportar.me'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato via site de:  $name";
-$email_body = "<div>Mensagem de contato do https://aportar.me recebida.</div>"."<div><br><b>Detalhes:</b></div><div><b>Nome:&nbsp;&nbsp;</b>".$name."</div><div><b>E-mail:&nbsp;&nbsp;</b>".$email_address."</div><div><b>Telefone:&nbsp;&nbsp;</b>".$phone."</div><div><b>Mensagem:</b></div><div><code>".$message."</code></div>";
+$email_body = "<div>Mensagem de contato do https://aportar.me recebida.</div>"."<div><br><b><u>Detalhes</u>:</b></div><div><b>Nome:&nbsp;&nbsp;</b>".$name."</div><div><b>E-mail:&nbsp;&nbsp;</b>".$email_address."</div><div><b>Telefone:&nbsp;&nbsp;</b>".$phone."</div><div><b>Mensagem:</b></div><div><code>".$message."</code></div>";
 $headers = "From: contato@aportar.me\n";
 $headers .= "Reply-To: $email_address\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
