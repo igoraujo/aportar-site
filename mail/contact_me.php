0dc66747720139a40b5c0cf7667307471d3cda1a
Igor Araújo <igor.araujo@fourtime.com>
2018-12-29 01:37:49 -0200

    fix utf-8 email
    fix utf-8 email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -21,10 +21,10 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'contato@aportar.me'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato via site de:  $name";
-// $email_subject = '=?UTF-8?B?'.base64_encode($email_subject).'?=';
 $email_body = "Mensagem de contato do https://aportar.me recebida.\n\n"."Detalhes:\n\nNome: $name\n\nE-mail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
 $headers = "From: contato@aportar.me\n";
-$headers .= "Reply-To: $email_address";   
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+$headers .= "Reply-To: $email_address";
 
 echo mail($to,$email_subject,$email_body,$headers);
 return true;
