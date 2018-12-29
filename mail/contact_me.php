f0088c752f16bd157ba7448e20651dfa712cda49
Igor Araújo <igor.araujo@fourtime.com>
2018-12-29 01:20:34 -0200

    fix UTF-8
    fix UTF-8

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,4 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||   
@@ -22,6 +21,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'contato@aportar.me'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato via site de:  $name";
+$email_subject = '=?UTF-8?B?'.base64_encode($email_subject).'?=';
 $email_body = "Mensagem de contato do https://aportar.me recebida.\n\n"."Detalhes:\n\nNome: $name\n\nE-mail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
 $headers = "From: contato@aportar.me\n";
 $headers .= "Reply-To: $email_address";   
