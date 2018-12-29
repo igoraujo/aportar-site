98b1c3946b5d4115fa33fdcd54cbc84c91d365fc
Igor Araújo <igor.araujo@fourtime.com>
2018-12-28 23:18:44 -0200

    removing phpmailer and dotenv
    removing phpmailer and dotenv

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,25 +2,28 @@
 
 // Check for empty fields
 if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
+   empty($_POST['email'])     ||   
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-   echo "No arguments Provided!";
+   echo "Nenhum argumento fornecido!";
    return false;
    }
-  
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
+if(!empty($_POST['phone'])){
+   $phone = strip_tags(htmlspecialchars($_POST['phone']));
+}else{
+   $phone="*Não informado";
+}
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'contato@aportar.me'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: contato@aportar.me\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_subject = "Contato via site de:  $name";
+$email_body = "Mensagem de contato do https://aportar.me recebida.\n\n"."Detalhes:\n\nNome: $name\n\nE-mail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
+$headers = "From: contato@aportar.me\n";
 $headers .= "Reply-To: $email_address";   
 
 echo mail($to,$email_subject,$email_body,$headers);
