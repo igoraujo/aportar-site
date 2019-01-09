ca070d8f971244ab71ac8118526cc94441aae749
Igor Araújo <igor.araujo@fourtime.com>
2019-01-09 19:03:39 -0200

    add redirect
    add redirect

--- index.php
+++ index.php
@@ -1289,9 +1289,9 @@
 
       let protocol = window.location.protocol;
       
-      // if(protocol.includes("http:")){
-      //   window.location = "https://aportar.me";
-      // }
+      if(protocol.includes("http:")){
+        window.location = "https://aportar.me";
+      }
 
       var SPMaskBehavior = function (val) {
         return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
