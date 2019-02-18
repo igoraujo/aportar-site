7423a8632258e51361db5b09873c54c6c3e9c60d
Igor Araujo <igoraujo93@gmail.com>
2019-02-17 21:23:15 -0300

    alter redirect http

--- index.php
+++ index.php
@@ -1387,9 +1387,10 @@
   <script>
     let protocol = window.location.protocol;
 
-    // if(protocol.includes("http:")){
-    //   window.location = "https://aportar.me";
-    // }
+    if(protocol.includes("http:")){
+      window.location = "https://aportar.me";
+    }
+
     var SPMaskBehavior = function (val) {
       return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
     },
@@ -1398,6 +1399,7 @@
           field.mask(SPMaskBehavior.apply({}, arguments), options);
         }
       };
+      
     $('.sp_celphones').mask(SPMaskBehavior, spOptions);
 
     $(document).ready(function () {
